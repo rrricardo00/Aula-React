d1fb2fe2b48a818d4a4f65c7309a40f6b0c0c0cb
expert99 <rrricardo00@yahoo.com.br>
2020-03-06 13:43:32 -0300

    aula react

---
+++ src/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => <h1>Primeiro Componente</h1>;
+
+
+
+// function primeiro(){
+
+//     return <h1>Primeiro Componente</h1>;
+
+// }
+
+// export default primeiro;
