95b7c86f8793e8b93602b991767e626c1c2ba64a
rrricardo00 <rrricardo00@yahoo.com.br>
2020-03-30 15:16:53 -0300

    aula

---
+++ src/componentes/doiscomponentes.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const CompA = props =>  <h1>Primeiro componente de 2</h1>  ;
+const CompB = props =>  <h1>Segundo compontente de 2</h1>;
+
+export {CompA, CompB}
+
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeiroComponente from './componentes/PrimeiroComponente';
+import {CompA, CompB}  from './componentes/doiscomponentes';
 
 const elemento = document.getElementById('root');
 ReactDOM.render(<div>
 
-<<<<<<< HEAD
-    <PrimeiroComponente valor="Esse é o atributo pros valor"></PrimeiroComponente>
-
-
-=======
-    <PrimeiroComponente valor="Esse é o atributo props do valor" attri="outro atributo"></PrimeiroComponente>
+    <CompA></CompA>
+    <PrimeiroComponente></PrimeiroComponente>
     
-    
->>>>>>> 524c528039cddda85f8edde592bfc6aa5d3198ce
 </div>, elemento);
 
